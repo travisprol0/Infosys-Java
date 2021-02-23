59cff8bcf4cd737183e9551d878cd007e5ab9344
travisprol0 <travisjohnprol@gmail.com>
2021-02-23 10:35:23 -0500

    assignments complete

---
+++ Exercises/Bill.java
@@ -0,0 +1,28 @@
+package Exercises;
+
+class Bill {
+    public double findPrice(int itemId){
+        
+    }
+}
+
+class Tester {
+
+	public static void main(String[] args) {
+		
+		Bill bill = new Bill();
+		
+		double price = bill.findPrice(1001);
+		if(price>0)
+		    System.out.println("Price of the selected item is $"+price);
+		else
+		    System.out.println("The Item Id is invalid");
+		
+		price = bill.findPrice("Reebok","T-shirt",34);
+		if(price>0)
+		    System.out.println("Price of the selected item is $"+price);
+		else
+		    System.out.println("The values are not valid");
+	}
+	
+}
