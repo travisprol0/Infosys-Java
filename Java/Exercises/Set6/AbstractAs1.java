291e999c3f946583702fe2f1d06c15761245d7c0
FelixBucket <FelixBucket@users.noreply.github.com>
2021-03-03 13:39:50 -0700

    Updated Abstract Assignment 1
    This thing was so messed up with the wrong percentages and rounding errors... But it is all fixed now!

--- Java/Exercises/Set6/AbstractAs1.java
+++ Java/Exercises/Set6/AbstractAs1.java
@@ -1,126 +1,107 @@
-abstract class Payment{
+abstract class Payment {
     private int customerId;
     protected String paymentId;
     protected double serviceTaxPercentage;
-    
-    public Payment(int customerId){
+
+    public Payment(int customerId) {
         this.customerId = customerId;
     }
-    
-    public int getCustomerId(){
+
+    public int getCustomerId() {
         return customerId;
     }
-    
-    public void setCustomerId(int customerId){
+
+    public void setCustomerId(int customerId) {
         this.customerId = customerId;
     }
-    
-    public String getPaymentId(){
+
+    public String getPaymentId() {
         return paymentId;
     }
-    
-    public void setPaymentId(String paymentId){
+
+    public void setPaymentId(String paymentId) {
         this.paymentId = paymentId;
     }
-    
-    public double getServiceTaxPercentage(){
+
+    public double getServiceTaxPercentage() {
         return serviceTaxPercentage;
     }
-    
-    public void setServiceTaxPercentage(double serviceTaxPercentage){
+
+    public void setServiceTaxPercentage(double serviceTaxPercentage) {
         this.serviceTaxPercentage = serviceTaxPercentage;
     }
-    
+
     public abstract double payBill(double amount);
 }
 
-class DebitCardPayment extends Payment{
+class DebitCardPayment extends Payment {
     private static int counter = 1000;
     private double discountPercentage;
-    
-    public DebitCardPayment(int customerId){
+
+    public DebitCardPayment(int customerId) {
         super(customerId);
         String paymentId = "D" + ++counter;
         super.setPaymentId(paymentId);
     }
-    
-    public static int getCounter(){
+
+    public static int getCounter() {
         return counter;
     }
-    
-    public static void setCounter(int counter){
+
+    public static void setCounter(int counter) {
         DebitCardPayment.counter = counter;
     }
-    
-    public double getDiscountPercentage(){
+
+    public double getDiscountPercentage() {
         return discountPercentage;
     }
-    
-    public void setDiscountPercentage(double discountPercentage){
+
+    public void setDiscountPercentage(double discountPercentage) {
         this.discountPercentage = discountPercentage;
     }
-    
-    public double payBill(double amount){
-        if(amount<=500){
-            super.setServiceTaxPercentage(1.03);
-            setDiscountPercentage(1.01);
+
+    public double payBill(double amount) {
+        if (amount <= 500) {
+            super.setServiceTaxPercentage(2.5);
+            setDiscountPercentage(1);
         }
-        if(amount <=1000){
-            super.setServiceTaxPercentage(1.05);
-            setDiscountPercentage(1.02);
-        }else if(amount > 1000){
-            super.setServiceTaxPercentage(1.06);
-            setDiscountPercentage(1.03);
+        else if (amount <= 1000 && amount > 500) {
+            super.setServiceTaxPercentage(4);
+            setDiscountPercentage(2);
+        } else if (amount > 1000) {
+            super.setServiceTaxPercentage(5);
+            setDiscountPercentage(3);
         }
-        return amount * super.getServiceTaxPercentage() / getDiscountPercentage();
+        return amount + (amount * (super.getServiceTaxPercentage()*.01)) - (amount * getDiscountPercentage() * 0.01);
     }
 }
 
-class CreditCardPayment extends Payment{
+class CreditCardPayment extends Payment {
     private static int counter = 1000;
-    
-    public CreditCardPayment(int customerId){
+
+    public CreditCardPayment(int customerId) {
         super(customerId);
         String paymentId = "C" + ++counter;
         super.setPaymentId(paymentId);
     }
-    
-    public static int getCounter(){
+
+    public static int getCounter() {
         return counter;
     }
-    
-    public static void setCounter(int counter){
+
+    public static void setCounter(int counter) {
         CreditCardPayment.counter = counter;
     }
-    
-    public double payBill(double amount){
-        if(amount<=500){
-            super.setServiceTaxPercentage(1.03);
-        } else if(amount <=1000){
-            super.setServiceTaxPercentage(1.05);
-        }else if(amount > 1000){
-            super.setServiceTaxPercentage(1.06);
+
+    public double payBill(double amount) {
+        if (amount <= 500) {
+            super.setServiceTaxPercentage(3);
+        } else if (amount <= 1000 && amount > 500) {
+            super.setServiceTaxPercentage(5);
+        } else if (amount > 1000) {
+            super.setServiceTaxPercentage(6);
         }
-        return amount * super.getServiceTaxPercentage();
+        return amount + (amount * (super.getServiceTaxPercentage()*0.01));
     }
 
-}
-
-class Tester{
-    public static void main(String args[]){
-        DebitCardPayment debitCardPayment = new DebitCardPayment(101);
-        double billAmount=Math.round(debitCardPayment.payBill(500)*100)/100.0;
-		System.out.println("Customer Id: " + debitCardPayment.getCustomerId());
-		System.out.println("Payment Id: " + debitCardPayment.getPaymentId());
-		System.out.println("Service tax percentage: " + debitCardPayment.getServiceTaxPercentage());
-		System.out.println("Discount percentage: " + debitCardPayment.getDiscountPercentage());
-		System.out.println("Total bill amount: " + billAmount);
-		
-		CreditCardPayment creditCardPayment = new CreditCardPayment(102);
-        billAmount=Math.round(creditCardPayment.payBill(1000)*100)/100.0;
-		System.out.println("Customer Id: " + creditCardPayment.getCustomerId());
-		System.out.println("Payment Id: " + creditCardPayment.getPaymentId());
-		System.out.println("Service tax percentage: " + creditCardPayment.getServiceTaxPercentage());
-		System.out.println("Total bill amount: " + billAmount);
-    }
 }
